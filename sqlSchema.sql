1d3f25c1fdbb60b01b8a8659d3d21fc0bf89220c
Rithvik <rra76@scarletmail.rutgers.edu>
2020-03-17 21:21:12 -0400

    Added some sql, got rid of template project, makes repo too messy
    SQL Not completed yet

--- sqlSchema.sql
+++ sqlSchema.sql
@@ -5,6 +5,8 @@ DROP TABLE IF EXISTS airlines;
 DROP TABLE IF EXISTS airports;
 DROP TABLE IF EXISTS reservations;
 DROP TABLE IF EXISTS preferences;
+DROP TABLE IF EXISTS operates;
+DROP TABLE IF EXISTS flights;
 
 CREATE TABLE preferences(
 pref_id int NOT NULL,
@@ -28,6 +30,22 @@ zip int NOT NULL,
 primary key(account_num)
 );
 
+CREATE TABLE flights (
+airline_id varchar(2) NOT NULL,
+flight_num INT NOT NULL,
+stops varchar(50) NOT NULL,
+fares INT NOT NULL,
+num_seats INT NOT NULL,
+working_days varchar(50),
+depart_time time NOT NULL,
+depart_aid varchar(2) NOT NULL,
+arrive_time time NOT NULL,
+arrive_aid varchar(2) NOT NULL,
+foreign key(depart_aid) references airports(Airport_id),
+foreign key(arrive_aid) references airports(Airport_id),
+foreign key(airline_id)references airlines (Airline_id),
+primary key (flight_num, airline_id)
+);
 CREATE TABLE airlines (
 Airline_id varchar(2) NOT NULL,
 a_name varchar(25) NOT NULL,
@@ -50,4 +68,12 @@ total_fare int NOT NULL,
 date_ date,
 fare_rest varchar(25) NOT NULL,
 primary key (res_number)
+);
+
+CREATE TABLE operates(
+Airline_id varchar(2) NOT NULL,
+flight_num int,
+primary key (Airline_id, flight_num),
+foreign key(flight_num) references flights(flight_num),
+foreign key(Airline_id) references airlines(Airline_id)
 );
