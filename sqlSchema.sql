1ab2bf37f68c6adaac7bfb5c3f8d8ea59743a3ef
Rithvik <rra76@scarletmail.rutgers.edu>
2020-04-21 17:28:43 -0400

    added some test data

--- sqlSchema.sql
+++ sqlSchema.sql
@@ -43,7 +43,6 @@ primary key(Airport_id));
 CREATE TABLE flights (
 airline_id varchar(2) NOT NULL,
 flight_num INT NOT NULL,
-stops varchar(50) ,
 fare_first DECIMAL(7, 2) NOT NULL,
 num_seats INT NOT NULL,
 depart_time time NOT NULL,
@@ -77,6 +76,11 @@ num_passengers INT,
 total_fare DECIMAL,
 PRIMARY KEY (res_num),
 FOREIGN KEY (customer) REFERENCES users (email)
+);
+
+CREATE TABLE Trips(
+res_num INT NOT NULL,
+FOREIGN KEY (res_num) REFERENCES Reservations(res_num)
 );
 
 /*insert manger accounts for login since manager account can't be created on the interface*/
@@ -142,20 +146,46 @@ INSERT INTO airlines Values('Virgin Australia Regional Airlines','VA');
 INSERT INTO flights VALUES('2 DIG airline id', '12345','STOPS','FARES', 'NUM_SEATS', 'DEPART_TIME', 'depart_AID', 'Arrive_time', 'arrive_aid', 'days', 'domestic?' );
 */
 /*Insertion of some domestic flights*/
-INSERT INTO flights VALUES ('BB', '12345', NULL, '160','50', '11:02', 'MIA','14:10', 'EWR', 'SuFSa', 'domestic' );
+INSERT INTO flights VALUES ('BB', '12345','160','50', '11:02', 'MIA','14:10', 'EWR', 'MTWRFSaSu', 'domestic' );
 INSERT INTO FlightDate VALUES('2020-05-05', '2020-05-06', '12345');
 
-INSERT INTO flights VALUES ('BB', '12346', NULL, '160','50', '11:02', 'EWR','14:10', 'MIA', 'SuFSa', 'domestic' );
+INSERT INTO flights VALUES ('BB', '12346','160','50', '11:02', 'EWR','14:10', 'MIA', 'MTWRFSaSu', 'domestic' );
 INSERT INTO FlightDate VALUES('2020-05-06', '2020-05-07', '12346');
 
+INSERT INTO flights VALUES ('UA', '01234', '250','200', '09:02', 'EWR', '14:10','LAX', 'MTWRFSaSu', 'domestic');
+INSERT INTO FlightDate VALUES('2020-05-05', '2020-05-06', '01234');
+
+INSERT INTO flights VALUES ('UA', '00123', '250','200', '09:02', 'LAX', '14:10','EWR', 'MTWRFSaSu', 'domestic');
+INSERT INTO FlightDate VALUES('2020-05-06', '2020-05-07', '00123');
+
+INSERT INTO flights VALUES ('UA', '19990', '250','210','09:02', 'JFK', '14:10','SFO', 'MTWRFSaSu', 'domestic');
+INSERT INTO FlightDate VALUES('2020-05-05', '2020-05-06', '19990');
+
+INSERT INTO flights VALUES ('UA', '19741', '250','210','09:02', 'SFO', '14:10','JFK', 'MTWRFSaSu', 'domestic');
+INSERT INTO FlightDate VALUES('2020-05-06', '2020-05-07', '19741');
 
 
 
+/*Insertion of domestic to international flights and international to domestic*/
+INSERT INTO flights VALUES ('BA', '86344', '900','210','09:02', 'JFK', '14:10','LCY', 'MTWRFSaSu', 'international');
+INSERT INTO FlightDate VALUES('2020-05-05', '2020-05-06', '86344');
 
+INSERT INTO flights VALUES ('BA', '57354', '900','210','09:02', 'LCY', '14:10','JFK', 'MTWRFSaSu', 'international');
+INSERT INTO FlightDate VALUES('2020-05-06', '2020-05-07', '57354');
 
+INSERT INTO flights VALUES ('AF', '64257', '950','210','09:02', 'JFK', '14:10','LCY', 'MTWRFSaSu', 'international');
+INSERT INTO FlightDate VALUES('2020-05-05', '2020-05-06', '64257');
 
+INSERT INTO flights VALUES ('AF', '79527', '950','210','09:02', 'LCY', '14:10','JFK', 'MTWRFSaSu', 'international');
+INSERT INTO FlightDate VALUES('2020-05-06', '2020-05-07', '79527');
 
+INSERT INTO flights VALUES ('VA', '85013', '940','210','09:02', 'EWR', '14:10','SYD', 'MTWRFSaSu', 'international');
+INSERT INTO FlightDate VALUES('2020-05-05', '2020-05-06', '85013');
 
+INSERT INTO flights VALUES ('VA', '97500', '940','210','09:02', 'SYD', '14:10','EWR', 'MTWRFSaSu', 'international');
+INSERT INTO FlightDate VALUES('2020-05-06', '2020-05-07', '97500');
+
+/*Insertion of international to international flights*/
 
 
 
